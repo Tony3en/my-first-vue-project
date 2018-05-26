9345c3de2c5a8154d38d72385323851439823da0
Tony3en <tony3en@gmail.com>
2018-05-26 22:50:29 +0800

    add: localStorage addfile : store.js

---
+++ src/store.js
@@ -0,0 +1,12 @@
+const STORAGE_KEY = 'todos-vuejs'
+export default {
+  fetch() {
+  	return JSON.parse(window.localStorage.getItem(STORAGE_KEY) ||'[]');
+  },
+  save(item){
+  	window.localStorage.setItem(STORAGE_KEY,JSON.stringify(item));
+  },
+  delete(){
+  	window.localStorage.removeItem(STORAGE_KEY)
+  }
+}
